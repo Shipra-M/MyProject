7a5ec4ffe6c784fa0e6a87814a95cce15861bd69
Shipra Mamgain <shipra09_mamgain@yahoo.co.in>
2020-05-24 11:40:36 +1000

    After Maven clean install.

---
+++ src/main/java/com/sm/MyProject/Shuffle.java
@@ -0,0 +1,28 @@
+package com.sm.MyProject;
+
+public class Shuffle{
+
+	public static void main(String[] args) {
+		int x = 3;
+		//System.out.println("X at start is " +x);
+		while(x>0){
+		if (x>2) {
+			System.out.print("a");
+			x=x-1;
+			System.out.print("-");
+		}
+		
+		if (x==2) {
+			System.out.print("b c");
+			x=x-1;
+			System.out.print("-");
+			}
+		if (x==1) {
+			System.out.print("d");
+			 x = x-1;
+		}
+		//System.out.println("X is " +x);
+		}
+	}
+
+}
